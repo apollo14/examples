d941b1eadf5866e87cf8f09cc1b0673bb9b732f0
JS <apollo14@o2.pl>
2018-09-28 14:34:43 +0200

    merge examples

---
+++ others/interview/src/main/java/collection/list/performance/ArrayListKontraLinkedList.java
@@ -0,0 +1,80 @@
+package collection.list.performance;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListKontraLinkedList {
+
+	private List<Integer> al = new ArrayList<Integer>();
+	private List<Integer> ll = new LinkedList<Integer>();
+
+	private long fill(int size, List<Integer> list) {
+
+		long start = System.nanoTime();
+		for (int i = 0; i <= size; i++) {
+			list.add(i);
+		}
+		return System.nanoTime() - start;
+	}
+	
+	private long addOne(int size, List<Integer> list) {
+		int index = Math.floorDiv(size, 2);
+		long start = System.nanoTime();
+		
+		list.add(index, 1);
+		
+		return System.nanoTime() - start;
+	}
+	
+	public static void main(String[] args) {
+		ArrayListKontraLinkedList t = new ArrayListKontraLinkedList();
+		// add to the eond of array
+		//t.testAdd();
+		
+		// add one in the middle
+		t.testAddOne();
+	}
+	
+	private void testAdd() {
+		testAdd(1000);
+		testAdd(10000);
+		testAdd(100000);
+		testAdd(1000000);
+		testAdd(5000000);
+		testAdd(10000000);
+		//testAdd(50000000);	
+	}
+	private void testAdd(int size) {
+		long alTime = fill(size, al);
+		long llTime = fill(size, ll);
+		System.out.println("testAdd: size=" + size);
+		System.out.println("ArrayList  - time=" + alTime);
+		System.out.println("LinkedList - time=" + llTime);
+		
+		this.al = new ArrayList<Integer>(); 
+		this.ll = new LinkedList<Integer>();
+	}
+	
+	private void testAddOne() {
+		testAddOne(1000);
+		testAddOne(10000);
+		testAddOne(100000);
+		testAddOne(1000000);
+		testAddOne(10000000);
+	}
+	private void testAddOne(int size) {
+		fill(size, al);
+		fill(size, ll);
+		
+		System.out.println("testAddOne: size=" + size);
+		long alTime = addOne(size, al);
+		System.out.println("ArrayList  - time=" + alTime);
+		long llTime = addOne(size, ll);
+		System.out.println("LinkedList - time=" + llTime);
+
+		this.al = new ArrayList<Integer>(); 
+		this.ll = new LinkedList<Integer>();
+
+	}
+}
---
+++ others/interview/src/main/java/collection/set/SetTest.java
@@ -0,0 +1,22 @@
+package collection.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+
+	public void addNullToHashSet() {
+		Set<Integer> s = new HashSet<>();
+		boolean r = s.add(null);
+		r = s.add(null);
+		r = s.add(1);
+		r = s.add(1);
+		System.out.println(s);
+	}
+	
+	public static void main(String[] args) {
+		SetTest st = new SetTest();
+		st.addNullToHashSet();
+		
+	}
+}
